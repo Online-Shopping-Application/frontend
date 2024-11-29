1dc052dd3d1e01c384de930d80b7911c0e563124
Mandara-samanalee <samanaleerktm.21@uom.lk>
2024-11-29 13:06:28 +0530

    initial commit

---
+++ routes/routes.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+
+function routes() {
+  return (
+    <div>
+      <Routes>
+{/*         <Route path="/login" element={<LoginPage />} /> */}
+      </Routes>
+    </div>
+  )
+}
+
+export default routes
