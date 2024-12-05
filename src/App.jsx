ed13a1fa363829b9404f4bf18960f6068d095316
Mandara-samanalee <samanaleerktm.21@uom.lk>
2024-12-05 11:36:07 +0530

    add changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import './App.css'
 import { BrowserRouter as Router } from 'react-router-dom'
 import AppRoutes from '../routes/routes'
-import Layout from './components/PageLayout/Layout'
 
 export default function App() {
   return (
